5599c5c3a797419f2f2d11e433beb34b24c5239f
homerobono <homerobono@gmail.com>
2016-05-20 21:40:43 -0300

    Add files via upload

--- profiles/deleteprofile.php
+++ profiles/deleteprofile.php
@@ -1,35 +1,19 @@
 <?php
 
-$users_file = file("users_login.dat");
-$user = $_GET['delete'];
-$userh = md5($user);
+$profile = $_GET['delete'];
+$profileh = md5($profile);
 
-#apagar registro de usuario e senha
-foreach($users_file as $user_key => $usr){
-    if(!strcmp($userh, substr($usr,0,32))){
-        unset($users_file[$user_key]);
-    }
-}
+$cad = simplexml_load_file('profiles.xml');
 
-$updated_file=fopen("users_login.dat","w");
-
-#atualizar o registro de usuario e senha
-foreach($users_file as $id){
-    fwrite($updated_file, $id);
-}
-fclose($updated_file);
-
-$cad = simplexml_load_file('usuarios.xml');
-
-#apagar as informacoes de cadastro
-foreach($cad->usuario as $usuario){
-    if(!strcmp($user, strval($usuario->usuario))){
-        unset($usuario[0]);
+#apagar as informacoes de cadastro de perfil
+foreach($cad->perfil as $perfil){
+    if(!strcmp($profile, strval($perfil->nome))){
+        unset($perfil[0]);
         break;
     }
 }
 #atualizar as informacoes de cadastro
-$cad->asXML('usuarios.xml');
+$cad->asXML('profiles.xml');
 
 echo "<p>" . "Done!" . "</p>";
 
--- profiles/profiledetails.php
+++ profiles/profiledetails.php
@@ -1,46 +1,47 @@
 <html>
 <head>
-<title>Usuários</title>
+<title>Perfis</title>
 </head>
 <basefont SIZE=4>
 <body TEXT="#000000" LINK="#0000ff" bgcolor="#FFFFFF">
-<H2><STRONG><I>Usuários </I></STRONG></H2>
+<H2><STRONG><I>Perfis </I></STRONG></H2>
 <P>
 <?php
 
-$usr = simplexml_load_file('usuarios.xml');
+$profiles = simplexml_load_file('profiles.xml');
 
-foreach ($usr->usuario as $user) {
-    if(!strcmp(strval($user->usuario),$_GET['detalhes'])){
-        echo ("<H2><STRONG>" . strval($user->nome) . 
+foreach ($profiles->perfil as $profile) {
+    if(!strcmp(strval($profile->nome),$_GET['detalhes'])){
+        echo ("<H2><STRONG>" . strval($profile->nome) . 
             "</STRONG></H2>");
         echo("<P>\n");
-        echo ("<STRONG>Usuário: </STRONG>" . 
-            strval($user->usuario));
+        echo ("<STRONG>Nome: </STRONG>" . 
+            strval($profile->nome));
         echo("<P>\n");
-        echo ("<STRONG>E-mail: </STRONG>" . 
-            strval($user->email));
+        echo ("<STRONG>Detalhes: </STRONG>" . 
+            strval($profile->detalhes));
         echo("<P>\n");
-        echo ("<STRONG>Endereço: </STRONG>" . 
-            strval($user->endereco));
+        echo "<STRONG>Permissões: </STRONG>";
         echo("<P>\n");
-        echo ("<STRONG>Telefone: </STRONG>" . 
-            strval($user->tel));
+        echo "Usuários: ";
+            strval($profile->usuarios);
         echo("<P>\n");
-        echo "\t<a href='deleteuser.php?delete=" . 
-            $user->usuario . "' target='principal'> Remover Usuário</a>\n";
-        echo "\t<a href='updateuser.php?mode=update&user="
-            . strval($user->usuario) . 
-            "&name=" . strval($user->nome) .
-            "&addr=" . strval($user->endereco) .
-            "&tel=" . strval($user->tel) .
-            "&email=" . strval($user->email) .
-            "' target='principal'> Editar Informações</a>\n";
+        echo ("Perfis: " . 
+            strval($profile->perfis));
+        echo("<P>\n");
+        echo "\t<a href='deleteprofile.php?delete=" . 
+            $profile->nome . "' target='principal'> Remover Perfil</a>\n";
+        echo "\t<a href='updateprofile.php?mode=update&profile="
+            . strval($profile->nome) . 
+            "&details=" . strval($profile->detalhes) .
+            "&users=" . strval($profile->usuarios) .
+            "&profiles=" . strval($profile->perfis) .
+            "' target='principal'> Editar Perfil</a>\n";
     }
 }
 ?>
 <P\n>
-<p align="left"><a href="showusers.php" target="">Listar Usuários</a></p>
-<p align="left"><a href="admin.php" target="">Pagina Inicial</a></p>
+<p align="left"><a href="showprofiles.php" target="">Listar Usuários</a></p>
+<p align="left"><a href="../admin.php" target="">Pagina Inicial</a></p>
 </body>
 </html>
